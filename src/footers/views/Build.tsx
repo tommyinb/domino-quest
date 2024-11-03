118b218d9a7cf6c2dbf01c638d87a3b1397593ce
Tommy <tommy@tom.my>
2024-11-04 01:44:27 +0800

    kindly change mode

--- src/footers/views/Build.tsx
+++ src/footers/views/Build.tsx
@@ -1,10 +1,47 @@
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { ControllerContext } from "../../controllers/ControllerContext";
 import { GestureMode } from "../../controllers/gestureMode";
+import { clicking } from "../../scenes/clicking";
+import { useGesture } from "../../scenes/useGesture";
 import "./Build.css";
 
 export function Build() {
   const { gestureMode, setGestureMode } = useContext(ControllerContext);
+
+  const [clicks, setClicks] = useState<number[]>([]);
+
+  useGesture(
+    useCallback(
+      (event) => {
+        if (clicking(event.pointers)) {
+          if (gestureMode === GestureMode.View) {
+            const time = Date.now();
+
+            const closeClicks = clicks.filter((click) => time - click < 3000);
+            if (closeClicks.length >= 3) {
+              setGestureMode(GestureMode.Build);
+              return true;
+            } else {
+              setClicks((clicks) => [...clicks, time]);
+              return false;
+            }
+          } else {
+            return false;
+          }
+        } else {
+          setClicks([]);
+          return false;
+        }
+      },
+      [clicks, gestureMode, setGestureMode]
+    )
+  );
+
+  useEffect(() => {
+    if (gestureMode === GestureMode.Build) {
+      setClicks([]);
+    }
+  }, [gestureMode]);
 
   return (
     <div
