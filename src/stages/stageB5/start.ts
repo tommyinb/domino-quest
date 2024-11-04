060bcda89e35e4f33461f0de62a4e9ee34b56a04
Tommy <tommy@tom.my>
2024-11-05 05:42:35 +0800

    stage title

--- src/stages/stageB5/start.ts
+++ src/stages/stageB5/start.ts
@@ -1,8 +1,8 @@
 import { Vector3Tuple } from "three";
 import { Stage } from "./Stage";
 
+export const name = "Bear Love";
+
 export const stageElement = Stage;
 export const stageHeight = 1000;
 export const cameraPosition: Vector3Tuple = [0, 450, 150];
-
-export const successMessage = "Bear";
